e6088aa70ac4c01c5925a15f48ca5f0d979aa62c
Lowik Chanussot <lowikchanussot@gmail.com>
2017-09-17 22:24:31 +0200

    Don't create symlinks anymore when building the dataset.
    But add a "path" column.

--- scripts/build_dataset.py
+++ scripts/build_dataset.py
@@ -10,26 +10,14 @@ import pandas as pd
 from cdiscount.utils.sampling import stratified_split, sample
 
 
-def create_item_symlink(x, img_dir, out_dir):
-    pid, iid, cid = x["product_id"], x["image_id"], x["category_id"]
-    cid_dir = os.path.join(out_dir, str(cid))
-    if not os.path.isdir(cid_dir):
-        os.makedirs(cid_dir)
-    img_name = "{}_{}.jpg".format(pid, iid)
-    src_name = os.path.join(img_dir, str(cid), img_name)
-    dst_name = os.path.join(out_dir, str(cid), img_name)
-    os.symlink(src_name, dst_name)
+def img_name(x):
+    cid, pid, iid = x["category_id"], x["product_id"], x["image_id"]
+    im_name = "{}_{}.jpg".format(pid, iid)
+    return os.path.join(str(cid), im_name)
 
 
-def create_symlinks(img_dir, out_dir, train, val):
-    out_train_dir = os.path.join(out_dir, "train")
-    out_val_dir = os.path.join(out_dir, "val")
-    os.makedirs(out_train_dir, exist_ok=True)
-    os.makedirs(out_val_dir, exist_ok=True)
-    train.apply(create_item_symlink, axis=1, args=(img_dir, out_train_dir))
-    val.apply(create_item_symlink, axis=1, args=(img_dir, out_val_dir))
-    logging.info("Symlinks created in {} (train) and {} (val)"
-                 .format(out_train_dir, out_val_dir))
+def add_path(df):
+    df["path"] = df.apply(img_name, axis=1)
 
 
 def main(args):
@@ -38,7 +26,8 @@ def main(args):
         sample(dataset, min_samples_per_class=args.min_samples,
                max_samples_per_class=args.max_samples, seed=args.seed)
     train, val = stratified_split(sampled, ratio=args.train_ratio)
-
+    add_path(train)
+    add_path(val)
     train_filename = os.path.join(args.out_dir, "train_{}_{}_{:.2f}_{}.feather"
                                   .format(args.min_samples, args.max_samples,
                                           args.train_ratio, args.seed))
@@ -49,9 +38,6 @@ def main(args):
     os.makedirs(args.out_dir, exist_ok=True)
     train.to_feather(train_filename)
     val.to_feather(val_filename)
-
-    if args.symlinks:
-        create_symlinks(args.img_dir, args.out_dir, train, val)
 
 
 if __name__ == "__main__":
@@ -76,8 +62,6 @@ if __name__ == "__main__":
     parser.add_argument('--train_ratio', type=float,
                         default=0.7,
                         help="Proportion of the kept samples in the train set.")
-    parser.add_argument('--symlinks', action='store_true',
-                        help="Create symlink to all images in train and val.")
     parser.add_argument('--seed', type=int,
                         default=42,
                         help="Seed for sampling")
